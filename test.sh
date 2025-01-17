64810bc02eee5aff03d5d775a722e753be124ce9
Ru Li <37427071+liru0126@users.noreply.github.com>
2025-01-17 15:29:18 +0800

    Add files

---
+++ test.sh
@@ -0,0 +1,4 @@
+python3 run_nerf.py \
+--config ./cartoonknight.txt \
+--test_checkpoint 2000000.tar \
+--render_only --render_test
---
+++ train.sh
@@ -0,0 +1,2 @@
+python3 run_nerf.py \
+--config ./cartoonknight.txt
